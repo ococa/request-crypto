bc1baa144780fff887025f622d89ac64bae76efa
ococa <wangchao.personal@gmail.com>
2024-03-11 18:47:10 +0800

    feat: update should encrypt function

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,4 @@ export { createRequestInstance } from './instance';
 export { createCryptoAxiosInstance } from './cryptoInstance';
 export { randomPassword } from './utils';
 export { getCryptoInfo, getSm4EncryptConfig } from './sm';
-export { isEncryptListApi } from './utils';
+export { shouldEncrypt } from './utils';
